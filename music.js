892cb9090389ea1869281f62d341a54b2bbc0948
FarisAsh <FarisTheCrafter@gmail.com>
2025-05-31 01:03:45 +0400

    Assignment 2 Web Dev Project

---
+++ music.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    if (!window._doomMusicInitialized) {
+        const audio = new Audio("pages/audio/doom_theme.mp3");
+        audio.loop = true;
+        audio.volume = 0.2;
+
+        //storing universally
+        window._doomAudio = audio;
+        window._doomMusicInitialized = true;
+
+        //only play when user interacts, this is limitation is due to browser restrictions
+        const startMusic = () => {
+            audio.play().catch(err => {
+                console.log("Autoplay blocked, waiting for interaction.");
+            });
+            document.body.removeEventListener("click", startMusic);
+        };
+
+        document.body.addEventListener("click", startMusic);
+
+        //resume music
+        const lastTime = localStorage.getItem("doom_music_time");
+        if (lastTime) {
+            audio.currentTime = parseFloat(lastTime);
+        }
+
+        //saving progress of the music
+        window.addEventListener("beforeunload", () => {
+            localStorage.setItem("doom_music_time", audio.currentTime);
+        });
+    }
+});
---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const quotes = [
+        '"They are rage, brutal, without mercy." - King Novik',
+        '"Rip and tear, until it is done." - King Novik',
+        '"Fight like Hell." - DOOM 2016 release tagline',
+        '"You think the only way is to kill them all - leave nothing behind - and you may be right." - Samuel Hayden',
+    ];
+
+    const quoteElement = document.getElementById('doom-quote');
+    let currentIndex = 0;
+
+    function updateQuote() {
+        //Fade out
+        quoteElement.style.opacity = 0;
+
+        setTimeout(() => {
+            //quote changes after fading out
+            quoteElement.textContent = quotes[currentIndex];
+            //Fade in
+            quoteElement.style.opacity = 1;
+
+            currentIndex = (currentIndex + 1) % quotes.length;
+        }, 500); //fade out duration
+    }
+
+    //starts with the first quote
+    updateQuote();
+    setInterval(updateQuote, 3000); //3 seconds for each quote
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const video = document.querySelector(".hover-video");
+
+    video.addEventListener("mouseenter", () => {
+        video.play();
+    });
+
+});
